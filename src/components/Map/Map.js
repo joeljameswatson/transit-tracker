d643a491542320a155360ad1ea878eaedddc9d79
Joel Watson <joeljameswatson@gmail.com>
2018-10-22 17:39:52 -0700

    resize map to viewport

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,19 +1,26 @@
 import React from "react";
 import ReactMapGL from "react-map-gl";
 import MarkerList from "./MarkerList";
+import AutoSizer from "react-virtualized/dist/commonjs/AutoSizer";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const Map = ({ mapState, onChangeViewport, busData }) => {
   return (
-    <ReactMapGL
-      {...mapState}
-      width={400}
-      height={400}
-      mapboxApiAccessToken={process.env.REACT_APP_MAP_BOX_API_TOKEN}
-      onViewportChange={onChangeViewport}
-    >
-      <MarkerList busData={busData} />
-    </ReactMapGL>
+    <AutoSizer>
+      {({ height, width }) => {
+        return (
+          <ReactMapGL
+            {...mapState}
+            width={width}
+            height={height}
+            mapboxApiAccessToken={process.env.REACT_APP_MAP_BOX_API_TOKEN}
+            onViewportChange={onChangeViewport}
+          >
+            <MarkerList busData={busData} />
+          </ReactMapGL>
+        );
+      }}
+    </AutoSizer>
   );
 };
 
