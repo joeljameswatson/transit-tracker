9c46ce805b96f249926813cbd3a7711642920a2e
Joel Watson <joeljameswatson@gmail.com>
2018-10-22 20:37:36 -0700

    fix performance issues using canvas overlay

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactMapGL from "react-map-gl";
-import MarkerList from "./MarkerList";
+import Immutable from "immutable";
 import AutoSizer from "react-virtualized/dist/commonjs/AutoSizer";
+import ScatterplotOverlay from "./ScatterPlotOverlay";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const Map = ({ mapState, onChangeViewport, busData }) => {
+  const busses = Immutable.fromJS(busData);
+
+  const lngLatAccessor = bus => {
+    return [bus.get("Longitude"), bus.get("Latitude")];
+  };
+
   return (
     <AutoSizer>
       {({ height, width }) => {
@@ -16,7 +23,15 @@ const Map = ({ mapState, onChangeViewport, busData }) => {
             mapboxApiAccessToken={process.env.REACT_APP_MAP_BOX_API_TOKEN}
             onViewportChange={onChangeViewport}
           >
-            <MarkerList busData={busData} />
+            <ScatterplotOverlay
+              locations={busses}
+              dotRadius={3}
+              globalOpacity={0.8}
+              compositeOperation="lighter"
+              dotFill="blue"
+              renderWhileDragging={true}
+              lngLatAccessor={lngLatAccessor}
+            />
           </ReactMapGL>
         );
       }}
